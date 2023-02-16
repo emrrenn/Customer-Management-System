472af0db6f1505ee741da2837a49f31347559fcc
emrrenn <emreren1224@gmail.com>
2023-02-16 22:55:23 +0300

    Main file changed

--- test_project.py
+++ test_project.py
@@ -1,13 +1,13 @@
-import project
+import main
 
 def test_check_username_password():
-    assert project.check_username_password("admin","1234") == False
-    assert project.check_username_password("adminn","123") == False
-    assert project.check_username_password("admin","12345") == True
+    assert main.check_username_password("admin","1234") == False
+    assert main.check_username_password("adminn","123") == False
+    assert main.check_username_password("admin","12345") == True
 
 def test_check_database():
-    assert project.check_database("customers.db") == True
-    assert project.check_database("customer.db") == False
+    assert main.check_database("customers.db") == True
+    assert main.check_database("customer.db") == False
 
 def test_check_database_elements():
-    assert project.check_database_elements() == project.total
+    assert main.check_database_elements() == main.total
